970f355f3944e5e8db9940fa5adc4d99fd67cbcb
Cristoffer Fairweather <developer@cfairweather.com>
2015-04-11 15:21:00 -0700

    Changed to UITab based controller. Rather important.

---
+++ consumer/Transient Watch/Transient Watch/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Transient Watch
+//
+//  Created by Cristoffer Fairweather on 4/11/15.
+//  Copyright (c) 2015 C Fairweather. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
