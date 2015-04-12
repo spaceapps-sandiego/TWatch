18c12607dfc408f0f9b2f0f97f626d0e71395a3d
Jeremy Jacobson <jjacobson93@gmail.com>
2015-04-12 12:08:51 -0700

    Made modifications for API token

--- provider/api.py
+++ provider/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify, make_response, abort
 import os
 
 NaN = float('nan')
@@ -6,7 +6,7 @@ NaN = float('nan')
 # initialize database
 from db import db, dbinsert, dbselectone
 import transients as trans
-from utils import convert_from_j2000
+from utils import convert_from_j2000, create_api_token
 
 API_TOKEN = None
 try:
@@ -15,6 +15,12 @@ try:
 	f.close()
 except IOError as e:
 	print 'No API token'
+	try:
+		f = open('provider/api_token', 'w+')
+		f.write(create_api_token())
+		f.close()
+	except IOError as e:
+		print 'Could not generate API token'
 
 api = Flask(__name__)
 
@@ -31,23 +37,26 @@ def transient_by_id(id):
 	row = trans.find_transient_by_id(id, lean=lean)
 	return jsonify({ 'result': row })
 
-@api.route('/api/v1/apn', methods=['POST'])
+@api.route('/api/v1/apn', methods=['GET', 'POST'])
 def apn():
-	device_token = request.values.get('deviceToken')
-	api_token = request.headers.get('X-API-Token')
+	if request.method == 'POST':
+		device_token = request.values.get('deviceToken')
+		api_token = request.headers.get('X-API-Token')
 
-	if API_TOKEN is None:
-		return make_response(jsonify({ 'error': 'This endpoint is currently disabled' }), 500)
+		if API_TOKEN is None:
+			return abort(404)
 
-	if device_token is None:
-		return make_response(jsonify({ 'error': '`deviceToken` is required' }), 400)
+		if device_token is None:
+			return make_response(jsonify({ 'error': '`deviceToken` is required' }), 400)
 
-	if api_token == API_TOKEN:
-		dbinsert(db, 'INSERT INTO devices (id, first_seen) VALUES (%s, NOW())', (device_token,))
-		row = dbselectone(db, 'SELECT * FROM devices WHERE id = %s', (device_token,))
-		return jsonify({ 'result': row })
+		if api_token == API_TOKEN:
+			dbinsert(db, 'INSERT INTO devices (id, first_seen) VALUES (%s, NOW())', (device_token,))
+			row = dbselectone(db, 'SELECT * FROM devices WHERE id = %s', (device_token,))
+			return jsonify({ 'result': row })
+		else:
+			return make_response(jsonify({ 'error': 'Invalid API Token' }), 400)
 	else:
-		return make_response(jsonify({ 'error': 'Invalid API Token' }), 400)
+		return abort(404)
 
 @api.route('/api/v1/convert', methods=['GET', 'POST'])
 def convert():
--- scripts/transients.sql
+++ scripts/transients.sql
@@ -9,33 +9,33 @@
 
 -- DROP TABLE IF EXISTS intensities;
 -- DROP TABLE IF EXISTS transients;
-DROP TABLE IF EXISTS devices;
+-- DROP TABLE IF EXISTS devices;
 
--- CREATE TABLE transients (
--- 	id INT NOT NULL AUTO_INCREMENT,
--- 	name VARCHAR(256) NOT NULL,
---   type VARCHAR(256),
--- 	ra VARCHAR(256) NOT NULL,
--- 	`dec` VARCHAR(256) NOT NULL,
--- 	PRIMARY KEY(ID),
---   FULLTEXT (name)
--- ) ENGINE=InnoDB;
+CREATE TABLE transients (
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL,
+  type VARCHAR(256),
+	ra VARCHAR(256) NOT NULL,
+	`dec` VARCHAR(256) NOT NULL,
+	PRIMARY KEY(ID),
+  INDEX name_idx (name)
+) ENGINE=InnoDB;
 
--- CREATE  TABLE intensities (
---   id INT NOT NULL AUTO_INCREMENT,
---   intensity DOUBLE NOT NULL,
---   error DOUBLE NOT NULL,
---   sigma DOUBLE NOT NULL,
---   trans_id INT NOT NULL,
---   detected_time INT NOT NULL,
---   PRIMARY KEY (id),
---   INDEX trans_id_idx (trans_id ASC),
---   CONSTRAINT trans_id
---     FOREIGN KEY (trans_id)
---     REFERENCES transients (id)
---     ON DELETE NO ACTION
---     ON UPDATE NO ACTION
--- ) ENGINE=InnoDB;
+CREATE  TABLE intensities (
+  id INT NOT NULL AUTO_INCREMENT,
+  intensity DOUBLE NOT NULL,
+  error DOUBLE NOT NULL,
+  sigma DOUBLE NOT NULL,
+  trans_id INT NOT NULL,
+  detected_time INT NOT NULL,
+  PRIMARY KEY (id),
+  INDEX trans_id_idx (trans_id ASC),
+  CONSTRAINT trans_id
+    FOREIGN KEY (trans_id)
+    REFERENCES transients (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+) ENGINE=InnoDB;
 
 CREATE TABLE devices (
   id VARCHAR(64) NOT NULL,
