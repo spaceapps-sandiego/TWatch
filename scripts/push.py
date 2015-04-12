c37e1786e15d5a6d013b2d1b51f2abb690b3d97a
nhirst100 <nhirst100@gmail.com>
2015-04-12 12:26:16 -0700

    update to add push notifications

--- scripts/push.py
+++ scripts/push.py
@@ -5,18 +5,23 @@
 
 import time
 from apns import APNs, Frame, Payload
+from db import db, dbselect
 
 apns = APNs(use_sandbox=True, cert_file='/opt/certs/twatch.crt', key_file='/opt/certs/twatch.pem')
 
-# Send a notification
-token_hex = '0c43fb33e4f4d1436905844876d3e04d0af2e565d0d09d3a97d7a07388bd39c4'
-payload = Payload(alert="Hey you - 'mere!", sound="default", badge=1)
-apns.gateway_server.send_notification(token_hex, payload)
+def send_push_to_all(notice):
+	# Send a notification
+	#token_hex = '0c43fb33e4f4d1436905844876d3e04d0af2e565d0d09d3a97d7a07388bd39c4'
+	payload = Payload(alert=notice, sound="default", badge=1)
+	tokens = dbselect(db, 'SELECT id FROM devices')
+	
+	for token in tokens:
+		apns.gateway_server.send_notification(token['id'], payload)
 
-# Send multiple notifications in a single transmission
-#frame = Frame()
-#identifier = 1
-#expiry = time.time()+3600
-#priority = 10
-#frame.add_item('0c43fb33e4f4d1436905844876d3e04d0af2e565d0d09d3a97d7a07388bd39c4', payload, identifier, expiry, priority)
-#apns.gateway_server.send_notification_multiple(frame)
+	# Send multiple notifications in a single transmission
+	#frame = Frame()
+	#identifier = 1
+	#expiry = time.time()+3600
+	#priority = 10
+	#frame.add_item('0c43fb33e4f4d1436905844876d3e04d0af2e565d0d09d3a97d7a07388bd39c4', payload, identifier, expiry, priority)
+	#apns.gateway_server.send_notification_multiple(frame)
