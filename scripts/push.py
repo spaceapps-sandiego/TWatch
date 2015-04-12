e11964591be3b84f748ac3e6954b432eac71b656
nhirst100 <nhirst100@gmail.com>
2015-04-12 12:49:46 -0700

    Modified push.py with db stuff

--- scripts/push.py
+++ scripts/push.py
@@ -5,7 +5,36 @@
 
 import time
 from apns import APNs, Frame, Payload
-from db import db, dbselect
+import MySQLdb as mdb
+import MySQLdb.cursors
+import sys
+
+db = None
+
+try:
+	db = mdb.connect('localhost', 'trans', 'transFTW!', 'trans', cursorclass=mdb.cursors.DictCursor)
+except mdb.Error, e:
+	print 'Error %d: %s' % (e.args[0], e.args[1])
+	sys.exit(1)
+
+def dbinsert(db, *args):
+	cur = db.cursor()
+	cur.execute(*args)
+	cur.close()
+
+def dbselectone(db, *args):
+	cur = db.cursor()
+	cur.execute(*args)
+	ret = cur.fetchone()
+	cur.close()
+	return ret
+
+def dbselect(db, *args):
+	cur = db.cursor()
+	cur.execute(*args)
+	ret = cur.fetchall()
+	cur.close()
+	return ret
 
 apns = APNs(use_sandbox=True, cert_file='/opt/certs/twatch.crt', key_file='/opt/certs/twatch.pem')
 
