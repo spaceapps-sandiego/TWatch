8a7f46711687049aed433207dd6955b5d82cb107
nhirst100 <nhirst100@gmail.com>
2015-04-12 21:40:25 -0700

    change from hard coded date to todays date

--- scripts/daily.py
+++ scripts/daily.py
@@ -1,21 +1,23 @@
 from scrape import *
 from push import send_push_to_all
+import datetime
 
 def run():
 	# get master data from Swift/BAT and update the database
 	table = get_master_table()
 	num_updated = update_master(table)
-	print num_updated
+	#print num_updated
 
 	if (num_updated>0):
 		sendstr = "There have been %d new events today" % (num_updated)
 		send_push_to_all(sendstr)
 
 	
-	knowns = get_knowns('150411')
+	knowns = get_knowns(datetime.date.today().strftime("%y%m%d"))
 
 	results = xref_master_daily(table, knowns)
 	insert_knowns(results)
 
 if __name__ == '__main__':
 	run()
+	
