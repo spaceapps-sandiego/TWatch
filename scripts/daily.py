e9fc03189ac14c6e60a40c57a7a1bbefc3bf73e2
nhirst100 <nhirst100@gmail.com>
2015-04-12 19:34:27 -0700

    checking updated

--- scripts/daily.py
+++ scripts/daily.py
@@ -5,7 +5,7 @@ def run():
 	# get master data from Swift/BAT and update the database
 	table = get_master_table()
 	num_updated = update_master(table)
-
+	print num_updated
 
 	if (num_updated>0):
 		send_push_to_all("There have been %n new events today", (num_updated))
