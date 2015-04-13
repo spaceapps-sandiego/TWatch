d93af4524f207417ee8546fc10a371ec1d49058c
nhirst100 <nhirst100@gmail.com>
2015-04-12 22:05:31 -0700

    comment out print

--- scripts/push.py
+++ scripts/push.py
@@ -45,7 +45,7 @@ def send_push_to_all(notice):
 	tokens = dbselect(db, 'SELECT id FROM devices')
 	
 	for token in tokens:
-		print ("sending %s to %s" % (notice, token['id']))
+		#print ("sending %s to %s" % (notice, token['id']))
 		apns.gateway_server.send_notification(token['id'], payload)
 
 	# Send multiple notifications in a single transmission
